dab1921aaa6f3c46b637e6ab114215f06867be61
fagneraureliano <fagner2aureliano@gmail.com>
2026-01-19 16:36:18 -0300

    fix bow damage

--- throwables/character_bow/character_bow.gd
+++ throwables/character_bow/character_bow.gd
@@ -4,5 +4,9 @@ class_name CharacterBow
 
 
 func _on_body_entered(_body: Node2D) -> void:
+	if _body is BaseEnemy:
+		_body.update_health(damage, self)
+		# queue_free()
+
 	if _body is TileMapLayer or _body is TileMap or _body is StaticBody2D:
 		queue_free()
